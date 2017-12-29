5202d46322e7dee9109c26f9055c1bcf6232001a
entrptaher@gmail.com <entrptaher@gmail.com>
2017-12-29 23:58:43 +0600

    Documents

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const pRetry = require('p-retry');
 const PQueue = require('p-queue');
 
 let gQueue;
+/**
+ * Create/Update global queue from provided object
+ * @param  {Object} queue Options to pass to new Queue object or Queue Instance
+ * @return {Any}
+ */
 function createQueue(queue) {
   if (queue instanceof PQueue) {
     gQueue = queue;
@@ -10,6 +15,14 @@ function createQueue(queue) {
   }
 }
 
+/**
+ * Add function to queue
+ * @param {Object}   queue             an instance or Option to pass to new PQueue instance
+ * @param {Function} fn                Function to add to queue
+ * @param {Boolean}   breakCondition    Breaks immidietely if true
+ * @param {Object}   [retryCondition]   Option to pass to retry module
+ * @return {Promise}
+ */
 function addToQueue(
   queue,
   fn,
